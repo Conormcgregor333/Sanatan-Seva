e7e3b0d5f867d4336e24aa7041c9c4463fd2aae2
Siddharth Pareek <siddharth2000pareek@gmail.com>
2024-05-11 11:28:02 +0000

    Added lightbox JS

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function txttonum() {
   let num = "917000868902";
-
-  let whatsappURL = "whatsapp://send?phone=" + num;
+  let message = "Pranam Guruji";
+  let whatsappURL = "whatsapp://send?phone=" + num+'?text='+message;
   window.open(whatsappURL, "_blank");
 }
 
@@ -14,3 +14,31 @@ x.addEventListener("click", () => {
   cont.appendChild(crd);
 });
  */
+document.getElementById("vid1d").addEventListener("click",()=>{
+  vid1.open()
+})
+document.getElementById("vid2d").addEventListener("click",()=>{
+  vid2.open()
+})
+
+document.getElementById("vid3d").addEventListener("click",()=>{
+  vid3.open()
+})
+
+document.getElementById("vid4d").addEventListener("click",()=>{
+  vid4.open()
+})
+
+document.getElementById("vid5d").addEventListener("click",()=>{
+  vid5.open()
+})
+
+document.getElementById("vid6d").addEventListener("click",()=>{
+  vid6.open()
+})
+
+document.getElementById("vid7d").addEventListener("click",()=>{
+  vid7.open()
+})
+
+

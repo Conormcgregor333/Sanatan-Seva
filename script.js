78a97798b7a6f9482dcacda4d87b69ee502b41fe
Siddharth Pareek <siddharth2000pareek@gmail.com>
2024-05-23 18:16:53 +0000

    added auto date update

--- script.js
+++ script.js
@@ -53,5 +53,56 @@ document.getElementById("vidnew6").addEventListener("click",()=>{
 document.getElementById("vidnew7").addEventListener("click",()=>{
   vid14.open()
 })
+document.getElementById("vidnew8").addEventListener("click",()=>{
+  vid15.open()
+})
+
+function dateDiff(date1, date2) {
+  const years = date2.getFullYear() - date1.getFullYear();
+  const months = date2.getMonth() - date1.getMonth();
+  const days = date2.getDate() - date1.getDate();
+
+  let diffYears = years;
+  let diffMonths = months;
+  let diffDays = days;
+
+  if (diffDays < 0) {
+    diffMonths -= 1;
+    const prevMonth = new Date(date2.getFullYear(), date2.getMonth(), 0);
+    diffDays += prevMonth.getDate();
+  }
+
+  if (diffMonths < 0) {
+    diffYears -= 1;
+    diffMonths += 12;
+  }
+  let x = diffYears * 365 + diffMonths * 30 + diffDays;
+  if (x <= 31) {
+    return x + " days ago";
+  } else if (x < 12 * 30) {
+    return diffMonths + " months ago";
+  } else {
+    return diffYears + " years ago";
+  }
+}
+
+/* const date1 = new Date("2023-12-15");
+const date2 = new Date();
+
+const diff = dateDiff(date1, date2);
+console.log(diff);
 
 
+ */
+let posted=["2024-05-15","2024-05-16","2024-05-17","2024-05-18","2024-05-19","2024-05-20","2024-05-21","2024-05-23"]
+let counter=1
+posted.map((index)=>{
+  let z="day"+counter
+  const date1 =new Date(index);
+ 
+  const date2 = new Date();
+  let d=dateDiff(date1, date2)
+  document.getElementById(z).innerText=d;
+  counter+=1
+
+}) 
